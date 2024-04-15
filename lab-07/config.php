3db57d50d91131f04215fc52a9c710978dd44076
4-110-2 <4-110-2@nje.local>
2024-04-15 11:17:53 +0200

    lab-07

---
+++ lab-07/config.php
@@ -0,0 +1,8 @@
+<?php
+
+    define('DB_PASS', 'iW57//i1jONIEPWB');
+    define('DB_NAME', 'manfred');
+
+    /*
+    CREATE TABLE `manfred`.`users` (`id` INT NOT NULL AUTO_INCREMENT , `name` VARCHAR(50) NOT NULL , `email` VARCHAR(50) NOT NULL , `password` VARCHAR(200) NOT NULL , `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP , PRIMARY KEY (`id`)) ENGINE = InnoDB CHARSET=utf8 COLLATE utf8_general_ci;
+    */
